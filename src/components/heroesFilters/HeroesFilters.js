40ea33ac326630a408e3803f2f250d48b55a1481
Stanislav Sharov <sharon.stanislav19@gmail.com>
2025-08-08 02:23:10 +0200

    createFilter and комбінування reducers

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -13,10 +13,10 @@ import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
 
-    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state);
+    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
     const dispatch = useDispatch();
     const {request} = useHttp();
-
+    
     // Запрос на сервер для получения фильтров и последовательной смены состояния
     useEffect(() => {
         dispatch(filtersFetching());
--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,6 +2,7 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import { createSelector } from 'reselect';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
@@ -15,7 +16,33 @@ import './heroesList.scss';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
+    // const filteredHeroes = createSelector()
+
+    // const filteredHeroes = useSelector(state=>{
+    //     if(state.filters.activeFilter === 'all'){
+    //         return state.heroes.heroes;
+    //     }else{
+    //         return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
+    //     }
+    // })
+
+    const filteredHeroesSelector = createSelector(
+        (state)=> state.heroes.heroes,
+        (state)=> state.filters.activeFilter,
+        (heroes,activeFilter) => {
+            if(activeFilter === 'all'){
+                return heroes
+            }
+            else{
+                return heroes.filter(item => item.element === activeFilter)
+            }
+        }
+    )
+    
+
+
+    const filteredHeroes = useSelector(filteredHeroesSelector)
+    const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
